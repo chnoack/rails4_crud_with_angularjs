5a554fe908d428005927aa027560e462d5b1ebbd
Christian Noack <christian.noack@agile-methoden.de>
2016-06-27 14:38:02 +0200

    CSFR-Token handling with angularjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require angular
 //= require angular-route
 //= require angular-resource
 //= require angular-ui-bootstrap
-//= require turbolinks
 //= require_tree .
-
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20140818112518) do
 
-  create_table "addresses", force: true do |t|
+  create_table "addresses", force: :cascade do |t|
     t.string   "street1"
     t.string   "street2"
     t.string   "city"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20140818112518) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
