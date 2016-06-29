0d3d689e9859aa24cc6c6fea164655d49c18c7dd
Christian Noack <christian.noack@agile-methoden.de>
2016-06-29 09:42:42 +0200

    add german locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def get_translations
+    I18n.backend.set(:translations)[:de].to_json
+  end
+
 end
